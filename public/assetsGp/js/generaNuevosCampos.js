4191a2c9eab6d974679f18c60b59881449c11d5d
Salix8 <saulpachecotrilles@gmail.com>
2022-01-31 01:51:56 +0100

    nuevo pluging

--- public/assetsGp/js/generaNuevosCampos.js
+++ public/assetsGp/js/generaNuevosCampos.js
@@ -1,31 +1,31 @@
-
 $(document).ready(function(){
-    $.fn.generaNuevosCampos = function(etiqueta, nombreCampo, indice) {
-      return $(this).each(function(){
+
+  $.fn.generaNuevosCampos = function(etiqueta, nombreCampo, indice) {
+
+    return $(this).each(function(){
+      elem = $(this);
+      elem.data(`etiqueta`, etiqueta);
+      elem.data(`nombreCampo`, nombreCampo);
+      elem.data(`indice`, indice);
+      
+      elem.click(function(e){
+        e.preventDefault();
         elem = $(this);
-        elem.data(`etiqueta`, etiqueta);
-        elem.data(`nombreCampo`, nombreCampo);
+        etiqueta = elem.data(`etiqueta`);
+        nombreCampo = elem.data(`nombreCampo`);
+        indice = elem.data(`indice`);
+        textoInsertar = `<p> ${etiqueta} ${indice}: <br>
+        <input type="text" name="${nombreCampo} ${indice}"/></p>`;
+        indice ++;
         elem.data(`indice`, indice);
-        
-        elem.click(function(e){
-          e.preventDefault();
-          elem = $(this);
-          etiqueta = elem.data(`etiqueta`);
-          nombreCampo = elem.data(`nombreCampo`);
-          indice = elem.data(`indice`);
-          textoInsertar = `<p> ${etiqueta} ${indice}: <br>
-          <input type="text" name="${nombreCampo} ${indice}"/></p>`;
-          indice ++;
-          elem.data(`indice`, indice);
-          nuevoCampo = $(textoInsertar);
-          elem.before(nuevoCampo);
-        });
+        nuevoCampo = $(textoInsertar);
+        elem.before(nuevoCampo);
       });
-  
-    };
-  
-    $("#mascampos").generaNuevosCampos("Compra", "compra", 2);
-  });
-  
-  
-  
+    });
+
+  };
+
+  $("#mascampos").generaNuevosCampos("Habilidad", "habilidad", 1);
+});
+
+
