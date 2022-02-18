be738fd8e648d6cff4b21cba9f72d9909b0d32ec
Salix8 <saulpachecotrilles@gmail.com>
2022-02-18 23:35:35 +0100

    Version acabada

--- public/assetsGp/js/autocompletado.js
+++ public/assetsGp/js/autocompletado.js
@@ -1,16 +1,31 @@
 $(document).ready(()=>{
-    $( function() {
-        function log( message ) {
-          $( "<div>" ).text( message ).prependTo( "#log" );
-          $( "#log" ).scrollTop( 0 );
-        }
-     
-        $( "#birds" ).autocomplete({
-          source: "search.php",
-          minLength: 2,
-          select: function( event, ui ) {
-            log( "Selected: " + ui.item.value + " aka " + ui.item.id );
-          }
-        });
+  $( function() {
+    var availableTags = [
+      "Galathar",
+      "Asmund",
+      "Phill",
+      "Bella",
+      "Kirito",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
     });
+  } );
 });
